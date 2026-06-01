6775e1d501909eabd6e86f4d630e90ec0464a980
vke1011 <vke10116@gmail.com>
2026-06-01 13:31:52 +0800

    Update version_service.dart

--- lib/services/version_service.dart
+++ lib/services/version_service.dart
@@ -4,8 +4,8 @@ import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class VersionService {
-  static const String githubRepoUrl = 'https://github.com/MoonTechLab/Selene';
-  static const String githubApiUrl = 'https://api.github.com/repos/MoonTechLab/Selene/releases/latest';
+  static const String githubRepoUrl = 'https://github.com/vke1011/Selene';
+  static const String githubApiUrl = 'https://api.github.com/repos/vke1011/Selene/releases/latest';
   static const String _lastCheckKey = 'last_version_check';
   static const String _dismissedVersionKey = 'dismissed_version';
   
